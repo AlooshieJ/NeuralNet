b2d13f43483643e13af7e5e906af4a7e7451553e
AlooshieJ <ajafar5@uic.edu>
2023-02-10 16:57:46 -0600

    wrapping predictor to work for n-order & some more lab2 additions

--- Homeworks/hw3.py
+++ Homeworks/hw3.py
@@ -7,23 +7,20 @@ ECE 491 - intro to neural networs Homework 3
 Programming linear predictor, calculating avg. error
 Mean Squared Error: MSE =
 (1/N) * (SUM) i = 1 -> n ( x^ [n] - x[n])^2 
-x^ is predicted  value, x[n] is true value 
 """
 
-# 2nd order adaptive filter NO Bias
+# n-order adaptive filter predictor class
 class Predictor:
-    def __init__(self,order=2, bias =0 ):
-        # self.Data = data
-        # self.n = len(data)
+    def __init__(self,order=2, bias =0 , debug = False ):
         self.bias = bias
         self.order= order
+        self.debug = debug
 
 
     # prediction for 2nd order uses x[n], x[n-1] to predict x[n+1]
     # X = [x1,x2] = [ x[n] , x[n-1] ]
     #returns output y[n] = dot product of X w/ weight
     def predict(self,X,W):
-        #print(X,W)
         return np.dot(X,W) + self.bias
 
     # return error squared
@@ -37,31 +34,41 @@ class Predictor:
         error_sum = 0
         w = w1
         error = 0
-        print("Input Data: ",xn,"\nInitial weight: ",w1,"\n-----------------")
+        if self.debug:
+            print("Input Data: ",xn,"\nInitial weight: ",w1,"\n-----------------")
 
         for i in range(len(xn)):
             #predict
-            X = xn[i:(i+1)+1]
-            prediction = self.predict(X,w) + self.bias
+            #X = xn[i:(i+1)+1]
+            X = xn[i:i+self.order]
+            prediction = self.predict(X,w)
+
+            # condition to get out of predictor loop
             try:
-                actual = xn[i+2]
+                actual = xn[i+self.order]
             except IndexError:
-                print("final Prediction:",prediction)
+
+                print("final Prediction:",prediction,"Final Weights: ",w,"Bias: ",self.bias)
                 break
             error = self.error_calc(prediction,actual)
             error_sum += (error**2)
 
             #updating weights
-            f = f"X: {X} prediciton: {prediction} | {actual}, w = {w},error:{error}"
-            print(f,end=" ")
+            if self.debug:
+                f = f"X: {X} prediciton: {prediction} | {actual}, w = {w},error:{error}"
+                print(f,end=" ")
             n = 1/ (np.linalg.norm(X))**2
             w = w +  n * np.dot(error,X)
-            print("updated w :",w)
-            print("----------------")
+            if self.debug:
+                print("updated w :",w)
+                print("----------------")
 
         mse = 1/len(xn) * error_sum
-        f = f"avg. error (MSE) = {mse}"
+        mse /= 100
+        # divide by 100 for the percent
+        f = f"avg. error (MSE) = {mse}%"
         print(f)
+        print("-----------------")
 
 
 
@@ -73,10 +80,24 @@ inputs = []
 for a in Data['Close/Last']:
     #print(a)
     inputs.append(float(a.replace('$','')))
+
 #reversing data, so that the earliest value is first
 #print(inputs)
 inputs.reverse()
-P = Predictor(bias = 1)
-#P.MSE(DataHW,[.5,.5])
-P.MSE(inputs,[1,1])
-#print(inputs)
+P = Predictor()
+P.MSE(DataHW,[.5,.5])
+
+
+# q 2a 2nd order predictor no bias
+Pa = Predictor()
+Pa.MSE(inputs,[1,1])
+
+# q 2b 2nd order predictor with bias
+Pb = Predictor(bias=1)
+Pb.MSE(inputs,[1,1])
+# q 2c 3rd order predictor no bias
+Pc = Predictor(order = 3)
+Pc.MSE(inputs,[1,1,1])
+# q 2d 3rd order predictor with bias
+Pd = Predictor(order= 3,bias = 15)
+Pd.MSE(inputs,[1,1,1])
--- LABS/Lab2/lab2_code.py
+++ LABS/Lab2/lab2_code.py
@@ -1,13 +1,14 @@
 import numpy as np
 import cv2 as cv
 import matplotlib.pyplot as plt
+from scipy.signal import convolve2d
 
 img =  cv.imread("boat.png", cv.IMREAD_GRAYSCALE)
 print(img.shape)
 f = plt.figure(1)
 ax =f.add_subplot(1,2,1)
 ax.set_title('Original Image')
-plt.imshow(img, cmap='gray')
+plt.imshow(img,cmap='gray')
 
 
 # converting the type for the image and normalize by 255
@@ -16,15 +17,33 @@ ax = f.add_subplot(1,2,2)
 ax.set_title('Normalized Image')
 plt.imshow(img,cmap='gray')
 
+
+plt.show()
+
 # boat -2 printing pixel value
 pixel = img[132,112]
 print(f"Pixel value @ (132,112) {pixel}")
 
-a ,b = 100,75
+a ,b = 0,200
 corner = img[a:b,a:b]
-ax = f.add_subplot(2,1,1)
-ax.set_title('top-left corner')
-plt.imshow(corner,cmap="gray")
+f = plt.figure(2)
+plt.imshow(corner,cmap= 'gray')
+
+
+plt.show()
+
+#3 linear filtering
+Ma = 1./9.*np.array([[1.,1.,1.,],[1.,1.,1.],[1.,1.,1.,]])
+img_filtered = convolve2d(img,Ma,mode='same')
+print("Filtered Image size/shape: ",img_filtered.shape)
+plt.figure(3)
+plt.subplot(1,2,1)
+plt.imshow(img,cmap='gray')
+
+plt.subplot(1,2,2)
+plt.imshow(img_filtered,cmap='gray')
+
+
 plt.show()
 
 
