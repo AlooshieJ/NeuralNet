8b8ee315ecedb524d4dd98eb85578c350f2321de
AlooshieJ <ajafar5@uic.edu>
2023-02-19 23:07:26 -0600

    more code for lab 3 up to converting labels from 0,1 to -1,1

--- LABS/Lab3/lab3Code.py
+++ LABS/Lab3/lab3Code.py
@@ -63,3 +63,71 @@ print("Train ims shape:",train_ims.shape)
 print("Train lbs shape:",train_lbs.shape)
 print("Test ims shape:",test_ims.shape)
 print("Test lbs shape:",test_lbs.shape)
+
+#5th sample from the data set from train_ims & train_lbs
+
+sample = train_ims[4]
+l_sample = train_lbs[4]
+t_img = np.reshape(sample, (NROWS, NCOLS))
+plt.figure(1)
+plt.imshow(t_img)
+print("sample label:",l_sample)
+
+# 5th sample from data set of test ims & test lbs
+sample = test_ims[4]
+l_sample = test_lbs[4]
+t_img = np.reshape(sample, (NROWS, NCOLS))
+plt.figure(2)
+plt.imshow(t_img)
+print("sample label:",l_sample)
+
+
+# Q4) only using samples of class 0 & 1
+
+mask = np.logical_or(train_lbs==0, train_lbs==1)
+train_ims = train_ims[mask,:]
+train_lbs = train_lbs[mask]
+
+print("filtering out all nums except 0&1 from train data:")
+print(train_ims.shape,train_lbs.shape)
+
+#Q5) doing the same filtering on the test data
+mask = np.logical_or(test_lbs==0, test_lbs==1)
+test_ims = test_ims[mask,:]
+test_lbs = test_lbs[mask]
+
+print("filtering out all nums except 0&1 from test data:")
+print(test_ims.shape,test_lbs.shape)
+
+
+#Q6) creating a validation data set
+
+val_ims = train_ims[:int(0.8*train_ims.shape[0]),:]
+train_ims = train_ims[int(0.8*train_ims.shape[0]):,:]
+
+val_lbs = train_lbs[:int(0.8*train_lbs.shape[0])]
+train_lbs = train_lbs[int(0.8*train_lbs.shape[0]):]
+
+print("training samples:")
+print(train_ims.shape,train_lbs.shape)
+
+print("validation samples:")
+print(val_ims.shape,val_lbs.shape)
+
+#Q7 type casting the datasets to float 32
+val_ims.astype(np.float32)
+val_lbs.astype(np.float32)
+
+train_ims.astype(np.float32)
+train_lbs.astype(np.float32)
+
+test_ims.astype(np.float32)
+test_lbs.astype(np.float32)
+
+#Q8) converting labels values from {0,1} to {-1,1} no loop
+#
+# val_lbs
+# train_lbs
+# test_lbs
+
+plt.show()
